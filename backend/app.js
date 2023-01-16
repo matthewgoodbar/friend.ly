01740b027150935ab2fac81513e4da3f32fd822b
matthew <matthewgoodbar@gmail.com>
2023-01-15 16:46:06 -0800

    CORS and CSRF protection

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const csurf = require('csurf');
 
+const cors = require('cors');
+const { isProduction } = require('./config/keys');
+
+const csrfRouter = require('./routes/api/csrf');
 const usersRouter = require('./routes/api/users');
 
 const app = express();
@@ -10,7 +15,21 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(
+    csurf({
+        cookie: {
+            secure: isProduction,
+            sameSite: isProduction && "Lax",
+            httpOnly: true
+        }
+    })
+);
 
+if (!isProduction) {
+    app.use(cors());
+}
+
+app.use('/api/csrf', csrfRouter);
 app.use('/api/users', usersRouter);
 
 module.exports = app;
