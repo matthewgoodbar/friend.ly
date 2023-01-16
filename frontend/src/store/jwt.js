fd1397117e7b6f37c52bb224df8bda9eef78b099
evgenii-shvetsov <sea.dev91@gmail.com>
2023-01-16 14:16:58 -0800

    add jwtFetch

--- frontend/src/store/jwt.js
+++ frontend/src/store/jwt.js
@@ -1,16 +1,28 @@
-async function jwtFetch(url, options = {}) {
+function getCookie(cookieName) {
+    const cookies = document.cookie.split(';');
+    for (let cookie of cookies) {
+        const [name, value] = cookie.split('=');
+        if (name.trim() === cookieName) return value;
+    }
+    return null;
+  }
+  
+  async function jwtFetch(url, options = {}) {
     // Set options.method to 'GET' if there is no method.
     options.method = options.method || "GET";
     // Set options.headers to an empty object if there is no headers.
     options.headers = options.headers || {};
     // Set the "Authorization" header to the value of "jwtToken" in localStorage.
-    options.headers["Authorization"] = localStorage.getItem("jwtToken");
-    
+    const jwtToken = localStorage.getItem("jwtToken");
+    if (jwtToken) options.headers["Authorization"] = 'Bearer ' + jwtToken;
+  
     // If the options.method is not 'GET', then set the "Content-Type" header to
-    // "application/json".
+    // "application/json" and the "CSRF-Token" header to the value stored in the
+    // "CSRF-TOKEN" cookie.
     if (options.method.toUpperCase() !== "GET") {
       options.headers["Content-Type"] =
         options.headers["Content-Type"] || "application/json";
+      options.headers["CSRF-Token"] = getCookie("CSRF-TOKEN");
     }
   
     // Call fetch with the url and the updated options hash.
