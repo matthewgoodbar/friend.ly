6b49f3c077e6c6f2b9f8a00fdd45ab91b027b161
evgenii-shvetsov <sea.dev91@gmail.com>
2023-01-16 14:29:31 -0800

    add main page and routes

---
+++ frontend/src/components/MainPage/MainPage.js
@@ -0,0 +1,12 @@
+function MainPage() {
+    return (
+      <>
+        <p>Friend.ly</p>
+        <footer>
+          Copyright &copy; 2022 Best team ever
+        </footer>
+      </>
+    );
+  }
+  
+  export default MainPage;
