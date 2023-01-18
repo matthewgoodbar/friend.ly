eb5be503207201cd7bcd7c89ecdac59d8d52904a
evgenii-shvetsov <sea.dev91@gmail.com>
2023-01-18 11:33:49 -0800

    complete messages left side bar

--- frontend/src/components/MessagesPage/MessagesPage.jsx
+++ frontend/src/components/MessagesPage/MessagesPage.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import NavBarSide from '../NavBarSide/NavBarSide'
+import MessagesLeftSideBar from '../MessagesLeftSideBar/MessagesLeftSideBar'
 import "./MessagesPage.css"
 
 const MessagesPage = () => {
   return (
       <div className='container messages'> 
-      {/* <div>MessagesPage</div> */}
-      <NavBarSide />
+            {/* <div>MessagesPage</div> */}
+            <NavBarSide />
+
+            <div className="content">
+                <MessagesLeftSideBar />
+            </div>
+
       </div>
   )
 }
