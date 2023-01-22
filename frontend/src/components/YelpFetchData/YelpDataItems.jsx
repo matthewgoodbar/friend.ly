5cb9c8c6db19da725bfc64bbc8f4995651794747
XiWang <vivian.xi.wang@gmail.com>
2023-01-21 22:16:16 -0800

    Get user location from state session

--- frontend/src/components/YelpFetchData/YelpDataItems.jsx
+++ frontend/src/components/YelpFetchData/YelpDataItems.jsx
@@ -3,15 +3,16 @@ import { Rating } from 'react-simple-star-rating'
 import { Link } from 'react-router-dom';
 import { getDistance } from 'geolib';
 import './yelp.css'
+import { useSelector } from 'react-redux';
 
 
 
 const YelpDataItems =({props}) => {
-
+    const user = useSelector(state => state.session.user)
     // props ={name:"Thai food", location:"San Francisco", radius: "5000", sort_by: "review_count"}
 
     const term = props.name
-    const location = 'San Francisco'
+    const location = user.location.zip
     const radius = 5000
     const sort_by = 'review_count' // options: rating, review_count,distance
 
@@ -39,7 +40,7 @@ const YelpDataItems =({props}) => {
             <div className='recommendation-head'
             style={{background: `url(${props.thumbnail}) 0 0 no-repeat`, backgroundSize: "cover"}}>
                 <div className='recommendation-interest'>{term}</div>
-                <div className='recommendation-location'>{location}</div>
+                <div className='recommendation-location'>{user.location.city}</div>
             </div>
             <div className='description'>{props.description}</div>
             <hr className='right-bar-line'/>
