99e138c28a85ff5032cbb5b72e245ff57347fc98
Marcos <jmarcoshenrich@gmail.com>
2023-02-04 12:03:53 -0800

    remove object deconstruction on changeuserchatroonm

--- frontend/src/store/chats.js
+++ frontend/src/store/chats.js
@@ -42,7 +42,7 @@ export const fetchUserChatrooms = (userId) => async dispatch => {
     }
 };
 
-export const changeUserChatroom = ({chatId}) => async dispatch => {
+export const changeUserChatroom = (chatId) => async dispatch => {
     try {
         const res = await jwtFetch(`/api/users/chatswap/`, {
             method: 'PATCH',
